6f83f026d4a07dd5d23618dd4da024ef95457f39
Thibault Hale <thibault.hale@gmail.com>
2020-01-13 16:44:25 +0100

    Page Qui somme nous / style.css

---
+++ quisommesnous.php
@@ -0,0 +1,45 @@
+<?php
+session_start();
+require('inc/pdo.php');
+require('function/function.php');
+require('inc/header.php')
+
+?>
+
+    <body>
+    <div id="groupe1"></div>
+    <h1 id="qui">Qui sommes nous ?</h1>
+    <img id="Bannieres" src="assets/css/img/Bannieres.jpg" alt="">
+    <h2 id="titreblabla">Nous avons 60 ans d'experiences dans le dev et oui jamy</h2>
+
+    <p id="blabla1">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad adipisci alias, aspernatur atque culpa
+        deserunt distinctio dolor eligendi, ipsam itaque libero omnis pariatur perferendis porro sapiente sit voluptas?
+        Ipsum, minus.</p>
+
+    <p id="blabla2">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab aliquid asperiores autem cumque
+        cupiditate delectus, deleniti deserunt dolor ea expedita explicabo inventore laboriosam nemo placeat quos sint
+        totam ut, vitae! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium aliquam animi culpa
+        cupiditate, dignissimos doloribus dolorum eum fuga fugiat iure laborum, natus nulla quae repellat repudiandae
+        sapiente suscipit tempore veniam. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab, architecto,
+        deleniti, doloribus excepturi harum iste laudantium maiores maxime minima nihil numquam obcaecati possimus sunt
+        suscipit tempore ut vel. Consequatur, ipsum?</p>
+
+    <div class="wrap"></div>
+
+    <div id="equipe">
+
+        <h3 id="equipetitre">Notre équipe .</h3>
+
+        <img id="etienne" src="assets/css/img/Etienne.jfif" alt="">
+
+        <img id="thibault" src="assets/css/img/Thibault.jfif" alt="">
+
+        <img id="victor" src="assets/css/img/Victor.jfif" alt="">
+
+    </div>
+
+
+    </body>
+
+<?php
+require('inc/footer.php'); ?>
