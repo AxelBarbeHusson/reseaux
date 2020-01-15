a02319ee74fed6cb953818c8ca39ee23a7619f6c
Axel Barbé--Husson <47109555+AxelVitch@users.noreply.github.com>
2020-01-15 10:08:42 +0100

    carte

--- inc/footer.php
+++ inc/footer.php
@@ -4,6 +4,9 @@
     crossorigin="anonymous"></script>
 <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/chart.js@2.8.0"></script>
+<script src="https://unpkg.com/leaflet@1.6.0/dist/leaflet.js"
+        integrity="sha512-gZwIG9x3wUXg2hdXF6+rVkLF/0Vi9U8D2Ntg4Ga5I5BZpVkVxlJWbSQtXPSiUTtC0TjtGOmxa1AJPuV0CPthew=="
+        crossorigin=""></script>
 <script src="assets/js/main.js"></script>
 </div>
 <footer>
